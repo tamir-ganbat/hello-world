8fee5e784830fa49d7cc66c56516e7ec0ec4e921
Tamir Ganbat <tamir.ganbat91@gmail.com>
2023-03-15 09:55:48 -0500

    sub branch change

--- src/main/FamilyTree.java
+++ src/main/FamilyTree.java
@@ -2,19 +2,7 @@ public class FamilyTree {
     public static void main(String[] args) {
         System.out.println("HERE IS LOG");
         /*
-        sdf
-        sdfsdf
-        sdfsdfsdf
-sdf
-
-        sdfsdf
-
-         System.out.println("HERE IS LOG");
-          System.out.println("HERE IS LOG");
-           System.out.println("HERE IS LOG");
-            System.out.println("HERE IS LOG");
-             System.out.println("HERE IS LOG");
-              System.out.println("HERE IS LOG");
-        Fixed conflicts and now ready merge*/
+        sub-branch change!!!
+        */ 
     }
 }
