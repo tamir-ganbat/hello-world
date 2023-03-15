b55b29a51fb3a96573616d93d5bee845f8bd29f8
Tamir Ganbat <tamir.ganbat91@gmail.com>
2023-03-14 23:54:50 -0500

    temp change

--- src/main/FamilyTree.java
+++ src/main/FamilyTree.java
@@ -1,5 +1,11 @@
 public class FamilyTree {
     public static void main(String[] args) {
         System.out.println("HERE IS LOG");
+        sdf
+        sdfsdf
+        sdfsdfsdf
+
+
+        sdfsdf
     }
 }
